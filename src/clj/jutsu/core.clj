c70e958a0142076f3f726c27bf2ecad7f3741440
hswick <hswick@smu.edu>
2017-08-17 13:12:54 -0500

    Added documentation to source code

--- src/clj/jutsu/core.clj
+++ src/clj/jutsu/core.clj
@@ -4,7 +4,14 @@
 ;;Initializes the jutsu server
 ;;Initializes the jutsu server router for sente
 ;;opens client in browser
-(defn start-jutsu! 
+(defn start-jutsu!
+  "
+  Initialize jutsu server.
+  [] will choose a random port number for you, and will try to display jutsu client
+  in default web browser.
+  [port display] port is an integer value to host jutsu on. display is a boolean value
+  to open up jutsu in browser or not.
+  "
   ([] (web/start!))
   ([port display] (web/start! port display)))
 
@@ -13,7 +20,10 @@
 ;;Data has to be vector should throw error if isnt
 ;;Should change meta-data to id
 (defn graph!
-  "Sends graph data to client to be visualized"
+  "Sends graph data to client to be visualized.
+  [id data] id is a string/numerical identifier for your graph.
+  data is the edn data you want to be plotted. This data must follow plotly.js format.
+  [id data layout] optional layout argument for global plotly config options."
   ([id data] (graph! id data {}))
   ([id data layout]
    (doseq [uid (:any @web/connected-uids)]
@@ -25,12 +35,23 @@
 
 ;;"Sends data to update graph with specified id"
 (defn update-graph!
+  "
+  Sends data to client to update graph specified by id.
+  [id data] id is a string/numerical identifier for the graph.
+  data is an edn data that must be formatted according to plotly conventions.
+  "
   [id data]
   (doseq [uid (:any @web/connected-uids)]
     (web/chsk-send! uid [:graph/update
                          {:id id
                           :data data}])))
 
-(defn dataset! [id data]
+(defn dataset!
+  "
+  Plot a table of clojure data to be viewed in a simple grid.
+  [id data] id is a string/numerical identifier for the table.
+  data is a tabular in shape dataset.
+  "
+  [id data]
   (doseq [uid (:any @web/connected-uids)]
     (web/chsk-send! uid [:dataset/dataset {:id id :data data}])))
